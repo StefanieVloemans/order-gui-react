f388fff5c52e07e6896038cded208896b3ffbf25
StefanieVloemans <stefanievloemans@gmail.com>
2022-05-18 08:30:56 +0200

    changed URL in client to heroku link + first attempt input validation

--- src/components/CreateCustomerForm.jsx
+++ src/components/CreateCustomerForm.jsx
@@ -36,15 +36,21 @@ function CreateCustomerForm() {
             <main>
                 <section className="container">
                     <h3 className="section-title mt-3 mb-3">Create Customer</h3>
-                    <form name="addCustomerForm">
+                    <form name="addCustomerForm needs-validation">
                         <div className="row mb-3">
                             <div className="col-6 ">
                                 <label className="form-label" htmlFor="firstName">Firstname</label>
-                                <input className="form-control mb-3 rounded-0" id="firstName" type="text" name="firstName" value={formValue.firstName} onChange={handleChange} />
+                                <input className="form-control mb-3 rounded-0" id="firstName" type="text" name="firstName" value={formValue.firstName} onChange={handleChange} required />
+                                <div className="invalid-feedback">
+                                    Please provide a first name
+                                </div>
                             </div>
                             <div className="col-6 ">
                                 <label className="form-label" htmlFor="lastName">Lastname</label>
-                                <input className="form-control mb-3 rounded-0" id="lastName" type="text" name="lastName" value={formValue.lastName} onChange={handleChange} />
+                                <input className="form-control mb-3 rounded-0" id="lastName" type="text" name="lastName" value={formValue.lastName} onChange={handleChange} required />
+                                <div className="invalid-feedback">
+                                    Please provide a last name
+                                </div>
                             </div>
                         </div>
                         <div className="row">
@@ -52,10 +58,16 @@ function CreateCustomerForm() {
                                 <label className="form-label" htmlFor="email">Email</label>
                                 <div className="input-group">
                                     <input className="form-control mb-3 rounded-0"
-                                           aria-label="email" id="email" type="text" name="emailFirstPart" value={formValue.emailFirstPart} onChange={handleChange} />
+                                           aria-label="email" id="email" type="text" name="emailFirstPart" value={formValue.emailFirstPart} onChange={handleChange} required  />
+                                    <div className="invalid-feedback">
+                                        Please provide an email address
+                                    </div>
                                     <span className="input-group-text mb-3 rounded-0">@</span>
                                     <input className="form-control mb-3 rounded-0"
-                                           aria-label="server" id="server" type="text" name="emailSecondPart" value={formValue.emailSecondPart} onChange={handleChange} />
+                                           aria-label="server" id="server" type="text" name="emailSecondPart" value={formValue.emailSecondPart} onChange={handleChange} required  />
+                                    <div className="invalid-feedback">
+                                        Please provide an email address
+                                    </div>
                                 </div>
                             </div>
 
@@ -65,35 +77,54 @@ function CreateCustomerForm() {
                             <div className="col-3 ">
                                 <div className="input-group">
                                     <span className="input-group-text mb-3 rounded-0">+</span>
-                                    <input className="form-control mb-3 rounded-0" id="phoneCountryCode" type="tel" name="phoneCountryCode" value={formValue.phoneCountryCode} onChange={handleChange} />
+                                    <input className="form-control mb-3 rounded-0" id="phoneCountryCode" type="tel" name="phoneCountryCode" value={formValue.phoneCountryCode} onChange={handleChange} required  />
+                                    <div className="invalid-feedback">
+                                        Please provide a phone number
+                                    </div>
                                 </div>
                             </div>
                             <div className="col-9">
-                                <input className="form-control mb-3 rounded-0" id="phoneNumber" type="tel" name="phoneNumber" value={formValue.phoneNumber} onChange={handleChange} />
+                                <input className="form-control mb-3 rounded-0" id="phoneNumber" type="tel" name="phoneNumber" value={formValue.phoneNumber} onChange={handleChange} required  />
+                                <div className="invalid-feedback">
+                                    Please provide a phone number
+                                </div>
                             </div>
                         </div>
                         <div className="row">
                             <div className="col-9">
                                 <label className="form-label" htmlFor="street">Street</label>
-                                <input className="form-control mb-3 rounded-0" id="street" type="text" name="streetName" value={formValue.streetName} onChange={handleChange} />
+                                <input className="form-control mb-3 rounded-0" id="street" type="text" name="streetName" value={formValue.streetName} onChange={handleChange} required  />
+                                <div className="invalid-feedback">
+                                    Please provide a street name
+                                </div>
                             </div>
                             <div className="col-3">
                                 <label className="form-label" htmlFor="number">Number</label>
-                                <input className="form-control mb-3 rounded-0" id="number" type="text" name="houseNumber" value={formValue.houseNumber} onChange={handleChange} />
+                                <input className="form-control mb-3 rounded-0" id="number" type="text" name="houseNumber" value={formValue.houseNumber} onChange={handleChange} required  />
+                                <div className="invalid-feedback">
+                                    Please provide a house number
+                                </div>
                             </div>
                         </div>
                         <div className="row">
                             <div className="col-5">
                                 <label className="form-label" htmlFor="postalCode">Postalcode</label>
-                                <input className="form-control mb-3" id="postalCode" type="text" name="postalCode" value={formValue.postalCode} onChange={handleChange} />
+                                <input className="form-control mb-3" id="postalCode" type="text" name="postalCode" value={formValue.postalCode} onChange={handleChange} required  />
+                                <div className="invalid-feedback">
+                                    Please provide a postal code
+                                </div>
                             </div>
                             <div className="col-7">
                                 <label className="form-label" htmlFor="country">Country</label>
-                                <select id="country" className="form-select mb-3" name="country" value={formValue.country} onChange={handleChange}>
+                                <select id="country" className="form-select mb-3" name="country" value={formValue.country} onChange={handleChange} required >
+                                    <option selected disabled value="">Select a country</option>
                                     <option value="Belgium">Belgium</option>
                                     <option value="France">France</option>
                                     <option value="Germany">Germany</option>
                                 </select>
+                                <div className="invalid-feedback">
+                                    Please select a country
+                                </div>
                             </div>
                         </div>
                         <div className="row">
